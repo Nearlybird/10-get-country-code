d7b177a8757a989db795ac3ef23fd64bc957997e
Nearlybird <49175582+Nearlybird@users.noreply.github.com>
2019-05-01 19:08:54 -0600

    Update get_country_codes.py

--- get_country_codes.py
+++ get_country_codes.py
@@ -1,3 +1,8 @@
-def get_country_codes(param):
-    # Your code here. Maybe try pseudocoding this one out?
-    return ""
+def get_country_codes(prices):
+    c_codes = ""
+    new_prices = prices.split()
+    for code in new_prices:
+        new_code = code[:2]
+        c_codes += new_code+", "
+    c_codes = c_codes[:-2]    
+    return(c_codes)
